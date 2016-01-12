c0266a6692bd3a3dfd5e8c2b2a334e4893539130
Jorge Roncel <jorgeron1993@hotmail.com>
2016-01-12 11:29:28 +0100

    Modificaciones
    Algunas modificaciones para realizar pruebas

--- pruebaExamen/src/main/java/es/app/App.java
+++ pruebaExamen/src/main/java/es/app/App.java
@@ -4,10 +4,16 @@ package es.app;
  * Hello world!
  *
  */
+import org.apache.log4j.*; 
+
+
 public class App 
 {
+	static Logger log=Logger.getLogger(App.class); 
+
     public static void main( String[] args )
     {
+    	log.info("Returning 1");
         System.out.println( "Hello World!" );
     }
 }
